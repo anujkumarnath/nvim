a6d587f1499e7ad6e5d463f715afd3a9a8629db5
Anuj Kumar Nath <anujkumarnath@gmail.com>
2026-03-08 09:28:22 +0530

    change default indentation style

--- lua/options.lua
+++ lua/options.lua
@@ -6,16 +6,16 @@ o.cursorlineopt = 'both' -- to enable cursorline(matlab nahi pata)
 
 -- For RGS
 -- indentation
--- o.tabstop = 2 -- how tabs look
--- o.shiftwidth = 2 -- spaces used for autoindent or 'shift' + '>'
--- o.softtabstop = 2 --
--- o.expandtab = false -- /t -> #tabstop spaces
+o.tabstop = 2 -- how tabs look
+o.shiftwidth = 2 -- spaces used for autoindent or 'shift' + '>'
+o.softtabstop = 2 --
+o.expandtab = false -- /t -> #tabstop spaces
 
 -- For Game
-o.expandtab = true      -- Use spaces instead of tabs
-o.shiftwidth = 4        -- Indent by 4 spaces
-o.softtabstop = 4       -- Tab key inserts 4 spaces
-o.tabstop = 2           -- Display width of a tab is 2 columns
+-- o.expandtab = true      -- Use spaces instead of tabs
+-- o.shiftwidth = 4        -- Indent by 4 spaces
+-- o.softtabstop = 4       -- Tab key inserts 4 spaces
+-- o.tabstop = 2           -- Display width of a tab is 2 columns
 
 o.foldmethod = 'indent'
 o.foldlevel = 99
