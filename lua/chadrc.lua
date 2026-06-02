be38c40fa8d74056ee58ca148f0a69d20b52c49b
Anuj Kumar Nath <anujkumarnath@gmail.com>
2026-06-02 13:52:06 +0530

    small update

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,13 +1,14 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
   theme = "oxocarbon",
-  transparency = true,
+  transparency = false,
+  theme_toggle = { "oxocarbon", "one_light" },
 
   statusline = {
     theme = "minimal",
